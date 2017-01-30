86fc052df05d053f789ac2685fe065494ad6d052
Thomas Desvenain <thomas.desvenain@gmail.com>
2017-01-30 17:17:18 +0100

    Get ckeditor back
    Override p.a.w back by old richtext widget

--- src/virtualcollections/src/virtualcollections/interfaces.py
+++ src/virtualcollections/src/virtualcollections/interfaces.py
@@ -3,6 +3,7 @@ from zope import interface
 from plone.theme.interfaces import IDefaultPloneLayer
 
 #from virtualcollections import MessageFactory as _
+from z3c.form.interfaces import IFormLayer
 
 
 class IThemeSpecific(IDefaultPloneLayer):
@@ -11,6 +12,6 @@ class IThemeSpecific(IDefaultPloneLayer):
     """
 
 
-class ILayer(interface.Interface):
+class ILayer(IFormLayer):
     """Marker interface that defines a Zope 3 browser layer.
     """
