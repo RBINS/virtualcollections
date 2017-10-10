cfa54d6f7053e3fcab3dbab7753aa05c5ba54e94
Thomas Desvenain <thomas.desvenain@gmail.com>
2017-10-10 11:38:47 +0200

    get back default behavior for display of document by line

--- src/virtualcollections/src/virtualcollections/browser/viewlets.py
+++ src/virtualcollections/src/virtualcollections/browser/viewlets.py
@@ -3,15 +3,7 @@
 # -*- coding: utf-8 -*-
 __docformat__ = 'restructuredtext en'
 
-import logging
-
-from plone.memoize.instance import memoize
-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFCore.utils import _checkPermission
-from Products.CMFCore.utils import getToolByName
-
-from plone.app.layout.globals.interfaces import IViewView
 from plone.app.layout import viewlets
 from plone.app.layout.viewlets import content
 
@@ -32,14 +24,11 @@ class LogoViewlet(ViewletBase):
         portal = self.portal_state.portal()
         logoTitle = self.portal_state.portal_title()
         self.src = LOGO_ID
-        self.navigation_root_title = self.portal_state.navigation_root_title() 
+        self.navigation_root_title = self.portal_state.navigation_root_title()
 
 
 class DocumentBylineViewlet(ViewletBase, content.DocumentBylineViewlet):
     '''.'''
     index = ViewPageTemplateFile('document_byline.pt')
-
-    def show(self):
-        return True
 
 #from plone.app.layout.viewlets.common import ViewletBase
